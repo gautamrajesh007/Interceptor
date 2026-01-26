609848d739eb1536116270e50116f8cd5e23e0ce
gautamrajesh007 <gautamrdev7@gmail.com>
2026-01-26 12:27:51 +0530

    add PendingQuery record to manage query state and callbacks

---
+++ src/main/java/com/proxy/interceptor/dto/PendingQuery.java
@@ -0,0 +1,15 @@
+package com.proxy.interceptor.dto;
+
+import io.netty.buffer.ByteBuf;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public record PendingQuery(
+        Long id,
+        String connId,
+        ByteBuf originalMessage,
+        Consumer<ByteBuf> forwardCallback,
+        Consumer<String> rejectCallback,
+        Set<String> approvals,
+        Set<String> rejections
+    ) {}
