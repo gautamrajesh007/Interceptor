0d43b55cdd4617790bfd69c4884f5a818ac461bb
gautamrajesh007 <gautamrdev7@gmail.com>
2026-02-19 11:26:28 +0530

    fix(SecurityConfig): update frameOptions configuration for improved security

--- src/main/java/com/proxy/interceptor/config/SecurityConfig.java
+++ src/main/java/com/proxy/interceptor/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -35,7 +36,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers
-                .frameOptions(frame -> frame.sameOrigin()))
+                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .sessionManagement(session
                         -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
